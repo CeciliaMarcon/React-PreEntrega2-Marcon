795686eab79827dcf161e0580b96b7b6933e26e1
Cecilia <mc.marcon@hotmail.com>
2024-04-01 12:56:52 -0300

    Pre entrega 2

---
+++ vite-project/src/components/NavBar.jsx
@@ -0,0 +1,21 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { NavLink } from "react-router-dom";
+
+import { CartWidget} from "./CartWidget";
+
+export const NavBar = () => {
+  return (
+    <Navbar bg="primary" data-bs-theme="primary">
+      <Container>
+        <Navbar.Brand href="#home">Fashion Verse</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link to="/category/Vestidos" as ={NavLink}>Vestidos</Nav.Link>
+          <Nav.Link to="/category/Jeans" as ={NavLink}>Jeans</Nav.Link>
+        </Nav>
+        <CartWidget/>
+      </Container>
+    </Navbar>
+  );
+};
